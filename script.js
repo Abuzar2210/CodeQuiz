b8f10a4d41aa125d7410ac8f51b8b5ebcbcfee77
Abuzar <abuzar.cheema@live.com>
2021-07-24 18:17:27 -0400

    made cahnges to CSS file, fixed minor errors in js file, added event listener

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var timeElement = document.querySelector(".time")
 //qurstion answer var
 var question = document.querySelector("#question")
-var answers = document.querySelector("#answers")
+var options = document.querySelector("#options")
 // game score and game end var
 var gameScore = document.querySelector(".game-score")
 var finished = document.querySelector("#game-finished")
@@ -21,58 +21,31 @@ var timer;
 let quiz =[
     {
         question: "What is the file type for Javascript?",
-        answers:{
-            a: ".js",
-            b: ".java",
-            c: ".script",
-            d: ".css",
-            correctAnswers: "a"
-        },
+        options: [".js", ".java",".script", ".css"],
+        answer: ".js"
         
     },
     {
         question: "What do we call properties inside an object?",
-        answers:{
-            a: "Methods",
-            b: "Functions",
-            c: "Element Names",
-            d: "Key Value Pairs",
-            correctAnswers: "d"
-        },
+        options: ["Methods", "Functions", "Element Names", "Key Value Pairs"],
+        answer: "Key Value Pairs"
         
     },
     {
         question: "Which of these chocies allows duplicate elements?",
-        answers:{
-            a: "Set",
-            b: "List",
-            c: "All",
-            d: "None of the above",
-            correctAnswers: "b"
-        },
+        options: ["Set", "List", "All", "None of the above"],
+        answer: "List"
         
     },
     {
         question: "Which built in method removes the first element of an array?",
-        answers:{
-            a: "shift()",
-            b: "removeLast()",
-            c: "pop()",
-            d: "last()",
-            correctAnswers: "a"
-        },
-        
+        options: ["shift()", "removeLast()", "pop()", "last()"],
+        answer: "shift()"
     },
     {
         question: "ArrayList implements which of the following?",
-        answers:{
-            a: "List",
-            b: "RandomAccess",
-            c: "Cloneable",
-            d: "All of the above",
-            correctAnswers: "d"
-        },
-        
+        options: ["List", "RandomAccess", "Cloneable", "All of the above"],
+        answer: "All of the above"
     }
 ]
 
@@ -80,7 +53,7 @@ let quiz =[
 
  function startTime(){
      timeElement.setAttribute("style","display:block;");
-     answers.setAttribute("style","display:block;");
+     options.setAttribute("style","display:block;");
      question.setAttribute("style","display:block;");
      initials.setAttribute("style","display:none;");
      gameScore.setAttribute("style","display:none;");
@@ -93,7 +66,7 @@ let quiz =[
 
      timer = setInterval(function(){
          secondsLeft--;
-         timeElement.textContent = secondsLeft + "remaining time left until Game Ends";
+         timeElement.textContent = secondsLeft + " Remaining time left until Game Ends";
          
          if(secondsLeft <=0 || whichQuestion === quiz.length){
              clearInterval(timer);
@@ -112,15 +85,18 @@ let quiz =[
  // questions fucntion
 
  function showQuiz(){
-     question.textContent = quiz[whichQuestion].question; answers.textContent = "";
+     question.textContent = quiz[whichQuestion].question; 
+     options.textContent = "";
 
-     for( let i = 0; i < quiz[whichQuestion].answers.length; i++){
-         var newButton = document.createElement("button");
-         newButton.textContent = quiz[whichQuestion].answers[i];
-         newButton.setAttribute("data-id, i");
+     for( let i = 0; i < quiz[whichQuestion].options.length; i++){
+         
+        
+        var newButton = document.createElement("button");
+         newButton.textContent = quiz[whichQuestion].options[i];
+         newButton.setAttribute("data-id", i);
          newButton.addEventListener("click", evaluateAnswer);
          newButton.style.display = "block";
-         answers.append(newButton);
+         options.append(newButton);
      }
      return;
  }
@@ -131,13 +107,14 @@ let quiz =[
      console.log(quiz[whichQuestion].answer)
      if(event.target.textContent !== quiz[whichQuestion].answer){
          secondsLeft = secondsLeft -10;
-         answers.style.backgroundColor = "red"
+         options.style.backgroundColor = "red"
      }   else{
-         score++; answers.style.backgroundColor = "green";
+         score++; 
+         options.style.backgroundColor = "green";
      }
      question.textContent = "";
 
-     if(whichQuestion === quiz.length-1){
+     if(whichQuestion === quiz.length-1) {
          saveInitial();
          clearInterval(timer);
          sendMessage();
@@ -148,11 +125,11 @@ let quiz =[
      }
  }
 
- function saveInitial(){
+ function saveInitial() {
      startButton.disabled = false;
 
      timeElement.setAttribute("style","display:none;");
-     answers.setAttribute("style","display:none;");
+     options.setAttribute("style","display:none;");
      question.setAttribute("style","display:none;");
      initials.setAttribute("style","display:block;");
 
@@ -168,5 +145,32 @@ let quiz =[
 
      initials.append(userInitial);
      initials.append(submitButton);
-     
+
+     submitButton.addEventListener("click", saveScore);
+
+     function saveScore(event){
+         event.preventDefault();
+
+         var inputInitial = document.querySelector(".input");
+         var saveMyScore = {
+             score : score, 
+             inputInitial: inputInitial.value,
+         };
+         console.log(saveMyScore);
+         localStorage.setItem("saveMyScore", JSON.stringify(saveMyScore));
+         renderMessage();
+
+         function renderMessage(){
+             gameScore.setAttribute("style", "display:black");
+             var lastScore = JSON.parse(localStorage.getItem("saveMyScore"));
+             gameScore.textContent = `Score: ${lastScore.score} Initials: ${lastScore.inputInitial}`;
+
+
+         }
+     }
+
+
  }
+
+ startButton.addEventListener("click", startTime);
+
